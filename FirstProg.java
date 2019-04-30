e1ba77ec714ccca9f373b35dd4e0e11d6dd5107e
Dunweiz8157 <dunweiz8157@gmail.com>
2019-04-30 16:51:40 -0600

    ljthw

---
+++ FirstProg.java
@@ -0,0 +1,7 @@
+public class FirstProg {
+  public static void main(String[] args){
+    System.out.println("I, Dunwei Zheng, amdetermind to learn how to code.");
+    System.out.println("Today's date is Tuesday 4/30/2019");
+
+  }
+}
