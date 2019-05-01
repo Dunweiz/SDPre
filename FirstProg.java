0d33f52b5dbe21ade56b41139eaf7c9429b03c26
Dunweiz8157 <dunweiz8157@gmail.com>
2019-04-30 18:46:39 -0600

    gitignore update

--- FirstProg.java
+++ FirstProg.java
@@ -1,6 +1,6 @@
 public class FirstProg {
   public static void main(String[] args){
-    System.out.println("I, Dunwei Zheng, amdetermind to learn how to code.");
+    System.out.println("I, Dunwei Zheng, am determind to learn how to code.");
     System.out.println("Today's date is Tuesday 4/30/2019");
 
   }
